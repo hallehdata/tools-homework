a0a4eb71eefde2ceed70cff38d7cb492a39b7752
hallehdata <153334032+hallehdata@users.noreply.github.com>
2024-02-08 09:22:08 -0500

    Add files via upload

---
+++ tools homework.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Peer-graded_Assignment_Coursera
+
+# In IBM Datascience course, one part is Peer-graded assignmen. 
+
+# Data Science Languages:
+#     #list in MD
+# 
+#     1-Python.
+#     2-R
+#     3-SQL
+#     4-Java
+#     5-Julia
+#     6-JavaScript
+#     7-Scala
+#     8-C++
+#     
+
+# Data Science libararies are:
+#     
+# 
+# 
+#     1-TensorFlow.
+#     2-NumPy.
+#     3-SciPy.
+#     4-Pandas.
+#     5-Matplotlib.
+#     6-Keras.
+#     7-SciKit-Learn.
+#     8-PyTorch.
+#     9-Scrapy.
+#     10-BeautifulSoup.
+#     11-LightGBM.
+#     12-Ramp.
+#     13-Pipenv.
+#     14-Bob.
+#    
+#    
+# 
+
+# In[3]:
+
+
+x=5
+y=10
+z=x+2*y
+z
+
+
+# In[6]:
+
+
+hour_to_minute=500/60
+print(f"{hour_to_minute} hours")
+
+
+# 1. First item
+# 2. Second item
+# 3. Third item
+# 4. Fourth item
+
+# |Fruits | Quantity(Kg) | Price(In Rupee) | 
+# | ------ | ------ | ------ | 
+# | Apple | 2 |500|
+# |Orange | 1 |200|
+# | Mango | 3 |1000|
+# | Grapes | 2 |400|
+
+# Author is Tianxiao Xiao
+
+# In[7]:
+
+
+result = compile("3 + 4", "<string>", "eval")
+eval(result)
+
+
+# In[ ]:
+
+
+
+
