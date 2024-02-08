ce89f5ebb7c8085dbd5717a03a43956813b12f81
hallehdata <153334032+hallehdata@users.noreply.github.com>
2024-02-08 14:16:35 -0500

    Add files via upload

---
+++ tools homework(1).py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Peer-graded_Assignment_Coursera
+
+# In[3]:
+
+
+x=5
+y=10
+z=x+2*y
+z
+
+
+# In[6]:
+
+
+hour_to_minute=500/60
+print(f"{hour_to_minute} hours")
+
+
+# 1. First item
+# 2. Second item
+# 3. Third item
+# 4. Fourth item
+
+# |Fruits | Quantity(Kg) | Price(In Rupee) | 
+# | ------ | ------ | ------ | 
+# | Apple | 2 |500|
+# |Orange | 1 |200|
+# | Mango | 3 |1000|
+# | Grapes | 2 |400|
+
+# Author is Tianxiao Xiao
+
+# In[7]:
+
+
+result = compile("3 + 4", "<string>", "eval")
+eval(result)
+
+
+# # Execute it as a markdown cell
+# **IBM Datascience course, one part is Peer-graded assignmen.**  
+# 
+
+#     1.Python
+#     2.R
+#     3.SQL
+#     4.Scala
+#     5.Java
+#     6.C++
+#     7.Julia
+#     8.JavaScript
+#     9.PHP
+#     10.Go
+#     11.Ruby
+#     12.Visual Basic
+# 
+
+# **Authors are :**  
+# **Lakshmi Holla**  
+# **Malika Singla**  
+# 
+#     
+# 
+# 
+# 
+# 
+
+# In[1]:
+
+
+200//60
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python. 
+# 
+
+# In[ ]:
+
+
+
+
